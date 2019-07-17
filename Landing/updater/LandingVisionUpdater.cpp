9a1c07ac52ade8b8a83d9927c1367e7f70649ab9
JerryXu0129 <1071260758@qq.com>
2019-07-17 22:21:02 +0800

    完成Landing目标的绝对坐标

--- Landing/updater/LandingVisionUpdater.cpp
+++ Landing/updater/LandingVisionUpdater.cpp
@@ -1,14 +1,23 @@
 #include <iostream>
 #include "LandingVisionUpdater.h"
+#include <cmath>
+#include "LandingUpdater.h"
 using namespace std;
 #include <time.h>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
-using namespace cv;
 
 int clientID;
-int midx,midy;
+float midx,midy;
+Point p;
+float height=0.282;
+float rate=1.75;
+float offsetx;
+float offsety;
+simxFloat position[3];
+simxFloat angle[3];
+simxFloat tar_position[3];
 LandingVisionUpdater::LandingVisionUpdater(int client_id): LandingUpdater(Config::Instance()->getIntParam("LandingVisionUpdater", "TIME_STEP"), client_id)
 {
     clientID=client_id;
@@ -18,26 +27,28 @@ LandingVisionUpdater::LandingVisionUpdater(int client_id): LandingUpdater(Config
 void LandingVisionUpdater::update()
 {
     simxInt camera;
+    simxInt target;
     simxGetObjectHandle(clientID, "zed_vision0", &camera, simx_opmode_blocking);
+    simxGetObjectHandle(clientID, "land_plane", &target, simx_opmode_blocking);
     simxInt resolution[2];
     simxUChar * image=0;
-    while(true){
-        //Sleep(1000);
-        //simxSetJointTargetVelocity(clientID, leftmotor, -time*0.02, simx_opmode_oneshot);
-        //simxSetJointTargetVelocity(clientID, rightmotor, -time*0.03, simx_opmode_oneshot);
-
+    while(true) {
         int ret = simxGetVisionSensorImage(clientID, camera, resolution, &image, 0, simx_opmode_oneshot);
         if (ret != simx_return_ok) {
-            continue;
+            return;
         }
-        Mat channel(resolution[1], resolution[0], CV_8UC3, image);
+        simxGetObjectPosition(clientID, camera, -1,position, simx_opmode_blocking);
+        simxGetObjectOrientation(clientID, camera, -1,angle,simx_opmode_blocking);
+//        cout << position[0] << " "<<position[1]<<" "<<position[2]<<"  "<<angle[0]<<" "<<angle[1]<<" "<<angle[2]<<endl;
+        cv::Mat channel(resolution[1], resolution[0], CV_8UC3, image);
         //读回来的图像数据是垂直翻转的,问题应该是在cvMat 和 v-rep 垂直坐标轴的方向相反,flip一下就正常了
         flip(channel, channel, 0);
-        int startx=-1,starty=-1;
-        int finishx=0,finishy=0;
-        for(int i=0;i<channel.rows;i++)
-            for(int j=0;j<channel.cols;j++)
-                if(channel.at<Vec3b>(i,j)[0]>230 && channel.at<Vec3b>(i,j)[1]>230 && channel.at<Vec3b>(i,j)[2]>230) {
+        int startx = -1, starty = -1;
+        int finishx = 0, finishy = 0;
+        for (int i = 0; i < channel.rows; i++)
+            for (int j = 0; j < channel.cols; j++)
+                if (channel.at<cv::Vec3b>(i, j)[0] > 230 && channel.at<cv::Vec3b>(i, j)[1] > 230 &&
+                    channel.at<cv::Vec3b>(i, j)[2] > 230) {
                     if (startx == -1) {
                         startx = i;
                         starty = j;
@@ -45,8 +56,15 @@ void LandingVisionUpdater::update()
                     finishx = i;
                     finishy = j;
                 }
-        midx=(startx+finishx)/2;
-        midy=(starty+finishy)/2;
-
+        midx = (startx + finishx) / 2;
+        midy = (starty + finishy) / 2;
+        offsetx=(position[2]-height)*rate/1280*(640-midy);
+        offsety=(position[2]-height)*rate/1280*(midx-360);
+        p.setX(position[0]+offsetx*cos(angle[2])+offsety*sin(angle[2]));
+        p.setY(position[1]+offsety*cos(angle[2])-offsetx*sin(angle[2]));
+        p.setZ(height);
+        simxGetObjectPosition(clientID, target, -1,tar_position, simx_opmode_blocking);
+        cout << p.x() << " "<<p.y()<<" "<<p.z()<<"  "<<tar_position[0]<<" "<<tar_position[1]<<" "<<tar_position[2]<<endl;
+        setTargetPosition(p);
     }
 }
