20f207784888c97c45b0a899c22acfb30b0658ed
ZYFZYF <2609108028@qq.com>
2019-07-24 13:13:08 +0800

    add StateControl for Landing again

--- Landing/updater/LandingVisionUpdater.cpp
+++ Landing/updater/LandingVisionUpdater.cpp
@@ -78,7 +78,7 @@ void LandingVisionUpdater::update() {
         p.setZ(height);
         tar_position = utils::getObjectPosition(target, m_cid);
         cout << p.x() << " " << p.y() << " " << p.z() << "  " << tar_position[0] << " " << tar_position[1] << " "
-             << tar_position[2] << endl;
+             << tar_position[2] <<' ' << angle[1] << ' ' << angle[2] << ' ' << angle[3] << endl;
         QRCode_pos = p;
         simxSetFloatSignal(clientID, "QRcode_x", p.x(), simx_opmode_oneshot);
         simxSetFloatSignal(clientID, "QRcode_y", p.y(), simx_opmode_oneshot);
