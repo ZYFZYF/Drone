432357cb217ab537a477fa0224460467bc37f0d3
fgj <fgj15@mails.tsinghua.edu.cn>
2019-07-26 07:49:38 +0800

    尝试加入一行抓取

---
+++ build/Following/CMakeFiles/Following.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Administrator/Desktop/Drone/Following/main.cpp" "C:/Users/Administrator/Desktop/Drone/build/Following/CMakeFiles/Following.dir/main.cpp.obj"
+  "C:/Users/Administrator/Desktop/Drone/Following/updater/FollowingPathUpdater.cpp" "C:/Users/Administrator/Desktop/Drone/build/Following/CMakeFiles/Following.dir/updater/FollowingPathUpdater.cpp.obj"
+  "C:/Users/Administrator/Desktop/Drone/Following/updater/FollowingUpdater.cpp" "C:/Users/Administrator/Desktop/Drone/build/Following/CMakeFiles/Following.dir/updater/FollowingUpdater.cpp.obj"
+  "C:/Users/Administrator/Desktop/Drone/Following/updater/FollowingVisionUpdater.cpp" "C:/Users/Administrator/Desktop/Drone/build/Following/CMakeFiles/Following.dir/updater/FollowingVisionUpdater.cpp.obj"
+  "C:/Users/Administrator/Desktop/Drone/Following/updater/PathUpdater.cpp" "C:/Users/Administrator/Desktop/Drone/build/Following/CMakeFiles/Following.dir/updater/PathUpdater.cpp.obj"
+  "C:/Users/Administrator/Desktop/Drone/Following/updater/VisionUpdater.cpp" "C:/Users/Administrator/Desktop/Drone/build/Following/CMakeFiles/Following.dir/updater/VisionUpdater.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "DO_NOT_USE_SHARED_MEMORY=1"
+  "MAX_EXT_API_CONNECTIONS=255"
+  "NON_MATLAB_PARSING=1"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "C:/Users/Administrator/Desktop/Drone/build/Common/CMakeFiles/common.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/Planning/CMakeFiles/Planning.dir/cmake_clean.cmake
@@ -0,0 +1,28 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Planning.dir/main.cpp.obj"
+  "CMakeFiles/Planning.dir/object/Cylinder.cpp.obj"
+  "CMakeFiles/Planning.dir/object/Door.cpp.obj"
+  "CMakeFiles/Planning.dir/object/Joint.cpp.obj"
+  "CMakeFiles/Planning.dir/object/Object.cpp.obj"
+  "CMakeFiles/Planning.dir/object/Platform.cpp.obj"
+  "CMakeFiles/Planning.dir/object/Tree.cpp.obj"
+  "CMakeFiles/Planning.dir/route/NaiveRouter.cpp.obj"
+  "CMakeFiles/Planning.dir/route/Obstacle.cpp.obj"
+  "CMakeFiles/Planning.dir/route/Router.cpp.obj"
+  "CMakeFiles/Planning.dir/task/GrabTask.cpp.obj"
+  "CMakeFiles/Planning.dir/task/MoveTask.cpp.obj"
+  "CMakeFiles/Planning.dir/task/Task.cpp.obj"
+  "CMakeFiles/Planning.dir/task/ThroughDoorTask.cpp.obj"
+  "CMakeFiles/Planning.dir/updater/PlanningPathUpdater.cpp.obj"
+  "CMakeFiles/Planning.dir/updater/PlanningUpdater.cpp.obj"
+  "CMakeFiles/Planning.dir/updater/PlanningVisionUpdater.cpp.obj"
+  "../../Planning/Planning.pdb"
+  "../../Planning/Planning.exe"
+  "../../Planning/Planning.exe.manifest"
+  "../../Planning/libPlanning.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Planning.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
