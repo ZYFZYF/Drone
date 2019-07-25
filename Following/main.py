4850ba20dc2169809544f8b594ec8a5a5c86e455
ZYFZYF <2609108028@qq.com>
2019-07-25 16:04:52 +0800

    stop simulation when ctrl-c is pressed

--- Following/main.py
+++ Following/main.py
@@ -6,12 +6,14 @@ if __name__ == '__main__':
     assert drone != 0
     assert target != 0
     vrep.simxStartSimulation(clientID, vrep.simx_opmode_blocking)
-    for i in range(9):
-        rotateDrone(10)
-        _, ret = vrep.simxGetObjectOrientation(clientID, target, -1, vrep.simx_opmode_blocking)
-        print(ret)
-        time.sleep(0.5)
-
-    # Now close the connection to V-REP:
-    vrep.simxStopSimulation(clientID, vrep.simx_opmode_oneshot)
-    vrep.simxFinish(clientID)
+    try:
+        for i in range(9):
+            rotateDrone(10)
+            _, ret = vrep.simxGetObjectOrientation(clientID, target, -1, vrep.simx_opmode_blocking)
+            print(ret)
+            time.sleep(0.5)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        vrep.simxStopSimulation(clientID, vrep.simx_opmode_blocking)
+        vrep.simxFinish(clientID)
