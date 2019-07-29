96d1f6af5623bbae424b1a90c005fca0dd44c724
ZYFZYF <2609108028@qq.com>
2019-07-29 11:39:08 +0800

    fix typo

--- Following/utils.py
+++ Following/utils.py
@@ -8,10 +8,10 @@ clientID = vrep.simxStart('127.0.0.1', 19997, True, True, 5000, 5)  # Connect to
 _, drone = vrep.simxGetObjectHandle(clientID, 'drone_zed', vrep.simx_opmode_blocking)
 _, target = vrep.simxGetObjectHandle(clientID, 'Quadricopter_target', vrep.simx_opmode_blocking)
 
-default_height = 2
+default_height = 3
 
 
-def calc_angel_by_xy(x, y):
+def calc_angle_by_xy(x, y):
     return math.atan2(y, x) / math.pi * 180
 
 
@@ -21,7 +21,7 @@ def set_target_position(pos):
     _, now_pos = vrep.simxGetObjectPosition(clientID, drone, -1, vrep.simx_opmode_blocking)
     delta_x = pos[0] - now_pos[0]
     delta_y = pos[1] - now_pos[1]
-    rotate_drone_to(calc_angel_by_xy(delta_x, delta_y))
+    rotate_drone_to(calc_angle_by_xy(delta_x, delta_y))
     vrep.simxSetObjectPosition(clientID=clientID,
                                objectHandle=target,
                                relativeToObjectHandle=-1,
@@ -29,19 +29,19 @@ def set_target_position(pos):
                                operationMode=vrep.simx_opmode_oneshot)
 
 
-def rotate_drone(angel):
+def rotate_drone(angle):
     vrep.simxSetObjectOrientation(clientID=clientID,
                                   objectHandle=target,
                                   relativeToObjectHandle=target,
-                                  eulerAngles=[0, 0, math.radians(angel)],
+                                  eulerAngles=[0, 0, math.radians(angle)],
                                   operationMode=vrep.simx_opmode_oneshot)
 
 
-def rotate_drone_to(angel):
+def rotate_drone_to(angle):
     vrep.simxSetObjectOrientation(clientID=clientID,
                                   objectHandle=target,
                                   relativeToObjectHandle=-1,
-                                  eulerAngles=[0, 0, math.radians(angel)],
+                                  eulerAngles=[0, 0, math.radians(angle)],
                                   operationMode=vrep.simx_opmode_oneshot)
 
 
@@ -67,7 +67,7 @@ def distance_between_drone_and_target():
     return np.linalg.norm(np.array([d1-d2 for d1, d2 in zip(drone_pos, target_pos)]))
 
 if __name__ == '__main__':
-    print(calc_angel_by_xy(0.1, 0))
-    print(calc_angel_by_xy(0, 0.1))
-    print(calc_angel_by_xy(-0.1, 0))
-    print(calc_angel_by_xy(0, -0.1))
+    print(calc_angle_by_xy(0.1, 0))
+    print(calc_angle_by_xy(0, 0.1))
+    print(calc_angle_by_xy(-0.1, 0))
+    print(calc_angle_by_xy(0, -0.1))
