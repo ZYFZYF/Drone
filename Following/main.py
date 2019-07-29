5cb72b991ab76bc780062f8677760247789128ea
Liyan Zheng <zhengly123@foxmail.com>
2019-07-29 11:16:22 +0800

    Add: wrap face detection and recognition function

--- Following/main.py
+++ Following/main.py
@@ -1,19 +1,32 @@
 # coding=utf-8
-from utils import *
+# from utils import *
+from facerecognition.contour_detection import *
+from facerecognition.recognition import *
 import time
+from matplotlib import pyplot as plt
 
-if __name__ == '__main__':
-    assert drone != 0
-    assert target != 0
-    vrep.simxStartSimulation(clientID, vrep.simx_opmode_blocking)
-    try:
-        for i in range(9):
-            rotateDrone(10)
-            _, ret = vrep.simxGetObjectOrientation(clientID, target, -1, vrep.simx_opmode_blocking)
-            print(ret)
-            time.sleep(0.5)
-    except KeyboardInterrupt:
-        pass
-    finally:
-        vrep.simxStopSimulation(clientID, vrep.simx_opmode_blocking)
-        vrep.simxFinish(clientID)
+# if __name__ == '__main__':
+#     assert drone != 0
+#     assert target != 0
+#     # vrep.simxStartSimulation(clientID, vrep.simx_opmode_blocking)
+#     try:
+#         for i in range(9):
+#             rotateDrone(10)
+#             _, ret = vrep.simxGetObjectOrientation(clientID, target, -1, vrep.simx_opmode_blocking)
+#             print(ret)
+#             time.sleep(0.5)
+#     except KeyboardInterrupt:
+#         pass
+#     finally:
+#         disconnect()
+
+def rec_image(img_rgb):
+    mask=color_mask(img)
+    contours=find_contours(mask)
+    faces=perspective_transformation(img, contours)
+    for i, face in enumerate(faces):
+        # plt.imshow(face)
+        # plt.savefig('output/%d.png'%i)
+        print(recogize_portrait(face))
+
+img=read_rgb_img('/home/eric/Work/Drone/Following/facerecognition/45dgrees-a1.png')
