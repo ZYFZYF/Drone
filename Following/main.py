ad5aebcc25311c0fd185769cc8c3d9b088579ae6
ZYFZYF <2609108028@qq.com>
2019-08-01 00:00:35 +0800

    speed up Following

--- Following/main.py
+++ Following/main.py
@@ -1,34 +1,13 @@
 # coding=utf-8
 from utils import *
 from path import path_update
-from facerecognition.contour_detection import *
-from facerecognition.recognition import *
-import time
-from matplotlib import pyplot as plt
 
-def rec_image(img):
-    mask=color_mask(img)
-    contours=find_contours(mask)
-    faces, boxes=perspective_transformation(img, contours)
-    for i, face in enumerate(faces):
-        # plt.imshow(face)
-        # plt.savefig('output/%d.png'%i)
-        print(recogize_portrait(face))
-        print(get_person_color(img, boxes[i]))
 
 if __name__ == '__main__':
     assert drone != 0
     assert target != 0
     vrep.simxStartSimulation(clientID, vrep.simx_opmode_blocking)
-    # vrep.simxSetObjectOrientation(clientID,target,base,{0,0,math.radians(90)},vrep.simx_opmode_oneshot)
-    # vrep.simxSetObjectOrientation(clientID,sub_target,target,{0,0,0},vrep.simx_opmode_oneshot)
     try:
-        # for i in range(9):
-        #     rotate_drone(10)
-        #     _, ret = vrep.simxGetObjectOrientation(clientID, target, -1, vrep.simx_opmode_blocking)
-        #     print(ret)
-        #     time.sleep(0.5)
-
         while True:
             path_update()
     except KeyboardInterrupt:
@@ -36,6 +15,3 @@ if __name__ == '__main__':
     finally:
         vrep.simxStopSimulation(clientID, vrep.simx_opmode_blocking)
         vrep.simxFinish(clientID)
-
-# img=read_rgb_img('/home/eric/Work/Drone/Following/facerecognition/45dgrees-a1.png')
-# rec_image(img)
--- Following/path.py
+++ Following/path.py
@@ -5,24 +5,28 @@ import numpy as np
 
 task_list = [[0, -4, 3.5, 8.5],
              [0, -4, 0, 8.5],
-             [0, -4, 0, 3],
+             [0, -4, 0],
              [90, -4, -9],
              [90, 10, -9],
              [90, 10, 0],
              [90, -4, 0]]
 
+for task in task_list:
+    if len(task) < 4:
+        task.append(default_height)
 now_index = 0
 last_time_found_people = time.time()
 is_ever_found_people = False
 
 
 def arrive_now_target():
-    distance = distance_between_drone_and_target()
+    # distance = distance_between_drone_and_target()
+    dis = distance(get_drone_position(), task_list[now_index][1:])
     res, vel, _ = vrep.simxGetObjectVelocity(clientID, base, vrep.simx_opmode_blocking)
     vel_norm = np.linalg.norm(vel)
-    print('now distance = ', round(distance,2),'\tvel =  ',round(vel_norm,2), ' ', now_index)
-    now_time = time.time()
-    return distance < 0.5 and vel_norm<0.5 
+    print('now distance = ', round(dis, 2), '\tvel =  ', round(vel_norm, 2), ' ', now_index)
+    return dis < 0.7 and vel_norm < 0.5
+
 
 def path_update():
     # 识别成功的情况下不作任何处理
@@ -42,4 +46,3 @@ def path_update():
                 now_index = 3
     else:
         rotate_drone(1)
-
